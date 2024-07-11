1356fb31adbe280f9c57a4b04bf44d2326ed0c9c
sracabanahashas <wkocsis@gmail.com>
2024-07-10 17:52:06 -0700

    Fix markCell function to correctly get value of targetCell and return if already marked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,7 +8,7 @@ function Gameboard() {
     for (let i = 0; i < rows; i++) {
         board[i]=[];
         for (let j = 0; j < columns; j++) {
-            board[i].push('');
+            board[i].push(Cell());
         }
     }
 
@@ -16,8 +16,8 @@ function Gameboard() {
 
     const markCell = (row, column, player) => {
         targetCell = board[row][column];
-        if (targetCell != '') return;
-        board[row][column].addMark(player);
+       if (targetCell.getValue() != 0) return;
+        targetCell.addMark(player);
     }
 
     const printBoard = () => {
@@ -26,6 +26,72 @@ function Gameboard() {
         cell.getValue()))
         console.log(boardWithCellValues);
     };
-    
-    return { board }
+
+    return { getBoard, markCell, printBoard }
 }
+
+function Cell() {
+    let value = 0;
+
+    const addMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMark,
+        getValue
+    };
+}
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+    ) {
+        const board = Gameboard();
+
+        const players = [
+            {
+                name: playerOneName,
+                mark: 1
+            },
+            {
+                name: playerTwoName,
+                mark: 2
+            }
+        ];
+
+        let activePlayer = players[0];
+
+        const switchPlayerTurn = () => {
+            activePlayer = activePlayer === players[0] ? 
+            players[1] : players[0];
+        };
+        const getActivePlayer = () => activePlayer;
+
+        const printNewRound = () => {
+            board.printBoard();
+            console.log(`${getActivePlayer().name}'s turn.`)
+        };
+
+        const playRound = (row, column) => {
+            console.log(
+                `Marking ${getActivePlayer().name}'s symbol at 
+                ${row}, ${column}...`
+            );
+            board.markCell(row, column, getActivePlayer().mark);
+    
+        switchPlayerTurn();
+        printNewRound();
+    };
+    
+    printNewRound();
+
+    return {
+    playRound,
+    getActivePlayer,
+    };
+}
+
+const game = GameController();
